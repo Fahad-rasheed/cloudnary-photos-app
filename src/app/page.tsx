8537a6b0140bc4f718789e843c2104de4252e058
fahad rasheed <dentixtfahad@gmail.com>
2024-01-23 19:09:16 -0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,36 +4,37 @@
 import { CldImage } from 'next-cloudinary';
 import { CldUploadButton } from 'next-cloudinary';
 import { useState } from 'react';
- 
-export type  UploadResult = {
-  info: {
-    public_id: string
-  },
-  
-  event: 'success',
-};
+
+interface UploadImage {
+  event: "success";
+  info: { public_id: string };
+}
 
 export default function Home() {
-    const [imageId, setImageId] = useState("");
-    
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <CldUploadButton
-          onUpload={(result: UploadResult) => {
-              setImageId(result.info.public_id);
+  //  use state to change Image
+  const [imageId, setImageId] = useState("");
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/*  Upload Button */}
+      <CldUploadButton
+        uploadPreset="wvn4e3g6"
+        onUpload={(result) => {
+          let res = result as UploadImage
+          setImageId(res.info.public_id);
         }}
-        
-        uploadPreset="wvn4e3g6" 
+
       />
-{imageId && (
-      <CldImage
-        width="400"
-        height="300"
-        src={imageId}
-        sizes="100vw"
-        alt="Description of my image"
-      />
-)}
+      {/* View Image */}
+      {imageId && (
+        <CldImage
+          width="400"
+          height="300"
+          src={imageId}
+          sizes="100vw"
+          alt="Description of my image"
+        />
+      )}
     </main>
   );
 }
